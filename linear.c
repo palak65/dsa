9e1ba78e14e470ef19c95ff673bbe54501efd86c
dharalunagariya <dharalunagariya5898@gmail.com>
2024-06-29 16:01:58 +0530

    changes

--- linear.c
+++ linear.c
@@ -2,33 +2,42 @@
 #include<stdlib.h>
 #define n 10
 int a[n]={10,2,3,45,675};
-
-int find_element(int x)
+int count=1;
+int find_element()
 { 
-    for(int i=0;i<=n-1;i++)
+    int x;
+    for (int i=0;i<n;i++)
+    {
+        a[i]=(rand()%50+1);
+    }
+    for (int i=0;i<n;i++)
+    {
+       printf("%d\t", a[i]);
+    }
+  
+    printf("\n select your elment:");
+    scanf("%d",&x);
+    for(int i=0;i<=n;i++)
     {
         if(a[i]==x)
         {
-            return i+1;
+             printf("Your Searching Elements Is Found On ==> %d <== Position \n", x);
+             break;
         }
-    }
-    return -1;
+        else {
+           count++;
+            }
+            }
+            printf("your searching data located %d num in the stack",count);
+   
 }
 
 int main()
 {
-    int i,j,k=45,l;
+    int i,j,k,l;
    
-    j=find_element(k);
-  if (j > 0)
-        {
-            printf("Your Searching Elements Is Found On ==> %d <== Position \n", j);
-          
-        }
-        else
-        {
-            printf("Your Searching Elements Is Not Found.\n");
-        }
+    find_element();
+ 
 
        
 }

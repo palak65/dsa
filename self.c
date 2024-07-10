b5bd6df536d4b93e024aa14105655e95773d9006
dharalunagariya <dharalunagariya5898@gmail.com>
2024-07-10 15:35:45 +0530

    commit

---
+++ self.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+struct node
+{
+    int data;
+    char ch;
+    struct node *ptr;
+};
+
+int main()
+{
+    struct node a,b,c,d;
+
+    a.data=100;
+    a.ch='A';
+    a.ptr=NULL;
+
+    b.data=500;
+    b.ch='B';
+    b.ptr=NULL;
+
+    c.data=400;
+    c.ch='C';
+    c.ptr=NULL;
+
+    
+
+    a.ptr=&b;
+    b.ptr=&c;
+   
+    
+    printf("B data:%d,char:%c\n",a.ptr->data,a.ptr->ch);
+    printf("C data:%d,char:%c\n",b.ptr->data,b.ptr->ch);
+   
+}
